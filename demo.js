f6930ffebb4e7b4dc59e32d064527ffd90692fb4
Dharmeshkatariya <katariyadharmesh658@gmail.com>
2023-10-01 13:59:59 +0530

    init commit

---
+++ demo.js
@@ -0,0 +1,9 @@
+console.log("hello")
+let a ="2";
+let c  = 2;
+// let a  = prompt("write a new value for a numbr put ");
+let b  = Number.parseInt(a);
+console.log(b);
+console.log(typeof a)
+console.log(typeof c)
+console.log(typeof b)
