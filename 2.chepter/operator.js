dc27c30b448c3365e07ee83b122402e7d9b2106f
Dharmeshkatariya <katariyadharmesh658@gmail.com>
2023-10-01 15:08:17 +0530

    all operator done

---
+++ 2.chepter/operator.js
@@ -0,0 +1,93 @@
+77;
+"Dharmeshs "
+false
+
+
+
+/// above that also be as variable apne aap me ek var he 
+
+console.log("Operators in Js")
+// Arithmetic Operators 
+
+
+let a = 10;
+let b = 4;
+
+// ++a and a++ what os diffrence
+// dif like as you print a++ for loop then a increment after thta in print
+// ++a is like first will be added in a value in a after that will be print  
+
+console.log(a++)
+
+/// `value incremnt 10+1 = 11  but not print 11  
+// and after use as ++a valu fist incremnt afetr tht will be print 
+// thats is diffrence in a++ or ++a
+console.log(++a)
+
+a = 10  ; 
+
+console.log("a + b = ", a + b)
+console.log("a - b = ", a - b)
+console.log("a / b = ", a / b)
+console.log("a ** b = ", a ** b)
+console.log("a % b = ", a % b)
+console.log("++a = ", ++a)
+console.log("a++ = ", a++)
+console.log("--a = ", --a)
+console.log("a-- = ", a--)
+console.log("a = ", a)
+console.log("a-- = ", a--)
+
+
+
+
+
+
+
+// Assignment Operators 
+let assignment = 1;
+assignment += 5                // same as assignment = assignment + 5
+assignment -= 5              
+assignment *= 5 
+assignment /= 5 
+
+
+
+// Comparison Operators 
+let comp1 = 6;
+let comp2 = 7;
+let comp3 = "7" ; 
+
+/// in  case you comarision in comp  2 and comp 3 that type will diffrent but 
+// Comparison for using  ==  that it give true and you can comparrision for  ===  that you can give as falsse 
+
+
+console.log(comp2 == comp3)
+console.log(comp2 === comp3)
+
+///  that is also b ediiffrence in == and ===  for js so kepe in mind to that is possible to use and ssome error 
+console.log(comp2 != comp3 )
+console.log(comp2 !==comp3)
+/// same thing 
+
+console.log("comp1 == comp2 is ", comp1 == comp2)
+console.log("comp1 != comp2 is ", comp1 != comp2)
+console.log("comp1 === comp2 is ", comp1 === comp2)
+console.log("comp1 !== comp2 is ", comp1 !== comp2)
+console.log("comp1 > comp2 is ", comp1 > comp2)
+
+// Logical Operators
+
+
+let x = 5;
+let y = 6;
+console.log(x<y && x==5)
+console.log(x>y || x==5)
+console.log(!false)
+console.log(!true)
+
+// Comments
+// This is a single line comment
+/* This is a sample multiline comment and a tutorial
+from 
+codewithharry */
